78b5492bf762ff36df9ec8d61b5dfd59857925e8
sho769 <shourya.vraman@gmail.com>
2023-08-27 22:00:04 +0530

    Simple PhotoFrame android application

---
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.PhotoFrame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    int[] s= new int[]{R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d};
+    int i=0;
+    public void nextImage(View view)
+    {
+        ImageView img = (ImageView)findViewById(R.id.imageView);
+        if(i<3) {
+            i++;
+            img.setImageResource(Integer.parseInt(String.valueOf(s[i])));
+        }
+        else
+        {
+            i=0;
+            img.setImageResource(Integer.parseInt(String.valueOf(s[i])));
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
